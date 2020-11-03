1fb26d6bbe5c979529a12f7b0c1716bab304027a
Collin Didier <collin.didier80@gmail.com>
2020-11-03 17:32:31 -0600

    pages - add notes

---
+++ single-page/src/pages/Home.js
@@ -0,0 +1,7 @@
+// Change default file names as needed.
+
+// Use each "page" component to assemble complete, standalone pages comprised of 
+// various subcomponents. 
+
+import React from 'react'
+import { NavLink } from 'react-router-dom'
