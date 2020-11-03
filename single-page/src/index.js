3f63bbebee15ee074c9f74934279d7e7eba127cd
Collin Didier <collin.didier80@gmail.com>
2020-11-03 17:31:24 -0600

    [index.js] add and reformat notes

--- single-page/src/index.js
+++ single-page/src/index.js
@@ -1,8 +1,3 @@
-// index.js is the highest javascript file in the heirarchy tree. It sets the React mode and renders the app content
-// created and structured in App.js, which is the 2nd highest file in the tree. In other words, the role of index.js
-// can be thought of as a configuration file using the React Dom, connecting to html, and rendering the app as a whole, 
-// whereas App.js builds out the content and html/jsx structure of the app. 
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
@@ -17,6 +12,19 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+
+//                        Overview of file/component hierarchy in a React-Browser project
+
+// index.js is the highest javascript file in the heirarchy tree. It sets the React mode, adds router functionality, 
+// and renders the app as built in App.js, which is the 2nd highest file in the tree. 
+// In other words, the role of index.js should be regarded as a configuration file using the React Dom, Browser Router, 
+// connecting to html, and rendering the app as a whole. App.js is responsible for setting routes and connecting 
+// them to their respective pages. Page components are used to assemble complete, standalone pages comprised of 
+// various subcomponents. 
+
+
+//                       How to start the application
 
 // initialize the app by entering command     >npm i   or     >npm install
 // use    > npm audit fix    if you receive warnings about vulnerable or deprecated files
